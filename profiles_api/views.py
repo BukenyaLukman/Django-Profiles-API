3946af101f7a381528968eb60f69b0010588baf5
BukenyaLukman <bukenyalukman@gmail.com>
2020-08-05 17:47:06 +0300

    Added improved permissions to use profile feed

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,12 +6,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
-
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 
 from . import serializers
 from . import models
-
 from . import permissions
 
 
@@ -112,3 +114,29 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 	permission_classes = (permissions.UpdateOwnProfile,)
 	filter_backends = (filters.SearchFilter,)
 	search_fields = ('name','email',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+	""" Check email and password and return an auth token"""
+	serializer_class = AuthTokenSerializer
+
+	def create(self,request):
+		""" Use the ObtainAuthToken APIVIEW to validate and create a token"""
+		return ObtainAuthToken().post(request)
+
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+	""" Handles creating, reading and updating profiles feed items"""
+	authentication_classes = (TokenAuthentication,)
+	serializer_class = serializers.ProfileFeeditemSerializer
+	queryset = models.ProfileFeedItem.objects.all()
+	permission_classes = (permissions.PostOwnStatus, IsAuthenticated)
+
+
+
+	def perform_create(self,serializer):
+		""" Sets the user profile to the logged in user """
+		serializer.save(user_profile=self.request.user)
+
+
